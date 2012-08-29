1405c7b92c838c114e497fae6addf9aeadef186a
henzil <henzil.jack@gmail.com>
2012-08-30 01:29:03 +0800

    测试使用express运行项目，新建html文件。

--- app.js
+++ app.js
@@ -3,43 +3,33 @@
  * Module dependencies.
  */
 
-//var express = require('express')
-//  , routes = require('./routes')
-//  , http = require('http')
-//  , path = require('path');
-//
-//var app = express();
-//
-//app.configure(function(){
-//  app.set('port', process.env.PORT || 4000);
-//  app.set('views', __dirname + '/views');
-//  app.set('view engine', 'jade');
-//  app.use(express.favicon());
-//  app.use(express.logger('dev'));
-//  app.use(express.bodyParser());
-//  app.use(express.methodOverride());
-//  app.use(app.router);
-//  app.use(express.static(path.join(__dirname, 'public')));
-//
-//
-//});
-//
-//app.configure('development', function(){
-//  app.use(express.errorHandler());
-//});
-//
-//app.get('/', routes.index);
-//
-//http.createServer(app).listen(app.get('port'), function(){
-//  console.log("Express server listening on port " + app.get('port'));
-//});
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 4000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 
 
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hello node.js! \n');
-}).listen(8080, "0.0.0.0");
-//听取1024以下的端口时，启动需要使用sudo node ***.js
+});
 
-console.log('Server running at http://127.0.0.1:80/');
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
