87cfdb200cdca10a5f41caccb848a1a89cf74f0f
henzil <henzil.jack@gmail.com>
2012-08-30 20:16:13 +0800

    重新制作tag09，上传一个文件，并放入public/images目录下。

--- app.js
+++ app.js
@@ -4,46 +4,48 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , ejs=require('ejs')
-  , fs = require('fs')
-  , path = require('path');
+    , routes = require('./routes')
+    , http = require('http')
+    , fs = require('fs')
+    , path = require('path');
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 4000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.logger({stream: fs.createWriteStream('./myLogFile.log', {flags: 'a'})}))
+    app.set('port', process.env.PORT || 4000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.logger({stream: fs.createWriteStream('./myLogFile.log', {flags: 'a'})}))
 
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server listening on port " + app.get('port'));
 });
 
-app.post('/saveUser', function(req,res){
-    var data=req.body;
-    var template='<html><body><h1>你好，<%=userName%></h1></body></html>';
-    //res.send('hello:'+req.body.userName);
-    res.send(ejs.render(template,data));
-    res.end();
-});
-app.get('/saveUser', function(req,res){
-    res.send('hello:'+req.query.userName);
+//TODO file 收到文件并储存
+app.post('/saveFile', function (req, res) {
+    console.log(req.files);
+
+    var tmp_path = req.files.file.path;
+    var target_path = './public/images/' + req.files.file.name;
+    fs.rename(tmp_path, target_path, function (err) {
+        if (err) throw err;
+        fs.unlink(tmp_path, function () {
+            if (err) throw err;
+        });
+    });
     res.end();
 });
