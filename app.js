d66c8b666c114a6ba2244b37be34536675600b92
henzil <henzil.jack@gmail.com>
2012-09-01 10:54:26 +0800

    修改了tag14 使用forever start 启动时，加入错误日志打印。

--- app.js
+++ app.js
@@ -36,15 +36,8 @@ http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
 });
 
-//TODO file 收到文件并储存
-app.post('/saveFile', function (req, res) {
-    console.log(req.files);
+app.get('/error', function(req, res) {
+    console.error('this is an error message');
 
-    var tmp_path = req.files.file.path;
-    var target_path = './public/images/' + req.files.file.name;
-    fs.rename(tmp_path, target_path, function (err) {
-        if (err) throw err;
-
-    });
     res.end();
 });
